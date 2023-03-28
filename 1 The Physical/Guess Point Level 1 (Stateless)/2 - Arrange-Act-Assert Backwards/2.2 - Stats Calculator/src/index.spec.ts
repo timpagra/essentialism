634d7da800c6f35ee56cb19fe5cd14d9f648e054
Timothy Atapagra <tim.atapagra@gmail.com>
2023-03-27 21:31:31 -0400

    [fix]: typo in test description

--- 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
+++ 1 The Physical/Guess Point Level 1 (Stateless)/2 - Arrange-Act-Assert Backwards/2.2 - Stats Calculator/src/index.spec.ts
@@ -6,9 +6,14 @@ describe('stats calculator', () => {
     statsCalculator = new StatisticsCalculator();
   });
 
-  it('should calculate minimum value in integer seq', () => {
+  it('should calculate average value in integer seq', () => {
     const numbers = [1, 2, 3, 4, 5];
     const result = statsCalculator.calculateAverage(numbers);
     expect(result).toEqual(3);
   });
 });
+
+// should accept an array of numbers
+// should return minimum value in the array
+// should return maximum value in the array
+// should return average value in the array
